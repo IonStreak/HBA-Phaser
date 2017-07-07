d41d9a5a207c1c81d623546b147dcd8b5e8b6acf
IonStreak <christopherl120@eagles.hba.net>
2017-07-07 13:38:57 -1000

    Add files via upload

--- HBA-Phaser/js/main.js
+++ HBA-Phaser/js/main.js
@@ -12,12 +12,12 @@ function preload(){
     game.load.json('level:1', 'data/level01.json');
     game.load.json('level:0', 'data/level00.json');
     //spawn platform sprites
-    game.load.image('ground', 'images/ground.png');
-    game.load.image('grass:8x1', 'images/grass_8x1.png');
-    game.load.image('grass:6x1', 'images/grass_6x1.png');
-    game.load.image('grass:4x1', 'images/grass_4x1.png');
-    game.load.image('grass:2x1', 'images/grass_2x1.png');
-    game.load.image('grass:1x1', 'images/grass_1x1.png');
+    game.load.image('ground', 'images/ground1.png');
+    game.load.image('grass:8x1', 'images/grass_8x1b.png');
+    game.load.image('grass:6x1', 'images/grass_6x1b.png');
+    game.load.image('grass:4x1', 'images/grass_4x1b.png');
+    game.load.image('grass:2x1', 'images/grass_2x1b.png');
+    game.load.image('grass:1x1', 'images/grass_1x1b.png');
 
     // load the hero image=
     game.load.image('hero', 'images/hero_stopped1.png');
@@ -25,9 +25,9 @@ function preload(){
     game.load.audio('sfx:coin', 'audio/coin.wav');
     game.load.audio('sfx:stomp', 'audio/stomp.wav');
     game.load.spritesheet('coin', 'images/coin_animated1.png', 22, 22);
-    game.load.spritesheet('spider', 'images/spider.png', 42, 32);
+    game.load.image('spider', 'images/spider1.png');
     game.load.image('invisible-wall', 'images/invisible_wall.png');
-    game.load.image('icon:coin', 'images/coin_icon.png');
+    game.load.image('icon:coin', 'images/coin_icon1.gif');
     game.load.image('font:numbers', 'images/numbers.png');
     game.load.spritesheet('door', 'images/door1.png', 42, 66);
     game.load.image('key', 'images/key1.png');
